0c65184db32b733d2e3cfa829d7794385efcd256
surendra_vidiyala <vidiyala.surendra@gmail.com>
2017-06-09 18:20:54 -0500

    updated config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl': 'mongodb://localhost:27017/anupama',
     'facebook': {
         clientID: '1568062700156807',
         clientSecret: '544ab167e466f9d84db62500e56d2233',
